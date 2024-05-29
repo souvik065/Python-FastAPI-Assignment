ac48c5c33ac7da94db19e479a255fe98f609c64f
souvik3950 <bhattacharyasouvik065@gmail.com>
2024-05-30 00:23:10 +0530

    Update User Function

--- routes/user.py
+++ routes/user.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-
+from bson import ObjectId
 from models.user import User
 from schemas.user import userEntity, usersEntity
 from  config.database import con
@@ -18,6 +18,13 @@ async def find_all_users():
 async def create_users(user: User):
     con.FastApi.user.insert_one(dict(user))
     return usersEntity(con.FastApi.user.find())
+
+@user.put('/{id}')
+async def update_user(id,user: User):
+    con.FastApi.user.find_one_and_update({"_id":ObjectId(id)},{"$set":dict(user)})
+    return usersEntity(con.FastApi.user.find_one({"_id":ObjectId(id)}))
+
+
 
 
 
